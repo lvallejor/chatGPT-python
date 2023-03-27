2c7076222e78ccf4aad6c527fc041e21e0158d80
David <lvallejo.cl@gmail.com>
2023-03-27 20:32:11 -0300

    listo para escalar

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import config
 openai.api_key = config.api_key
 
 response = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
-                                        messages=[{"role": "user", "content":"Que es el Banco BCI"}])
+                                            messages=[{"role": "user", "content":"Que es el Banco BCI"}])
 
 print(response.choices[0].message.content)
