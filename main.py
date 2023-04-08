4867e48370f2185428c722593989eb7d2f0238ce
Luis Vallejo <lvallejo.cl@gmail.com>
2023-04-08 17:27:40 -0400

    Chat e imagen

--- main.py
+++ main.py
@@ -15,17 +15,25 @@ def read_root():
     return {"healthcheck": "ok"}
 
 
-@app.post("/chatbot/", status_code=201)
+@app.post("/chatbot/", status_code=200)
 async def create_chat(content: str):
     contenido = content
     print(contenido)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{"role": "user", "content": contenido}])
-    print(response.choices[0].message.content)
-    return {"respuesta": response.choices[0].message.content}
+    response_chat = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                 messages=[{"role": "user", "content": contenido}])
+    print(response_chat.choices[0].message.content)
+    return {"respuesta": response_chat.choices[0].message.content}
 
 
-# response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
- #                                       messages=[{"role": "user", "content": "Que es el Banco BCI"}])
-
-# print(response.choices[0].message.content)
+@app.post("/imagen/", status_code=201)
+async def create_image(content: str):
+    contenido = content
+    print(contenido)
+    response_dallE = openai.Image.create(
+        prompt=contenido,
+        n=1,
+        size="256x256"
+    )
+    image_url = response_dallE['data'][0]['url']
+    print(image_url)
+    return {"Imagen": image_url}
