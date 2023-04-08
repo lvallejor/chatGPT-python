5e77fe3e1029e1274e11b798944c87184dc19c25
Luis Vallejo <lvallejo.cl@gmail.com>
2023-04-08 17:48:30 -0400

    ChatBot y Dalle-E

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ def read_root():
 async def create_chat(content: str):
     contenido = content
     print(contenido)
-    response_chat = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                 messages=[{"role": "user", "content": contenido}])
-    print(response_chat.choices[0].message.content)
-    return {"respuesta": response_chat.choices[0].message.content}
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                            messages=[{"role": "user", "content": contenido}])
+    response_chat = response.choices[0].message.content
+    print(response_chat)
+    return {"respuesta": response_chat}
 
 
 @app.post("/imagen/", status_code=201)
@@ -37,3 +38,21 @@ async def create_image(content: str):
     image_url = response_dallE['data'][0]['url']
     print(image_url)
     return {"Imagen": image_url}
+
+
+@app.post("/chat-image/", status_code=200)
+async def create_image(content: str):
+    contenido = content
+    print(contenido)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                            messages=[{"role": "user", "content": contenido}])
+    response_chat = response.choices[0].message.content
+    print(response_chat)
+    response_dallE = openai.Image.create(
+        prompt=contenido,
+        n=1,
+        size="512x512"
+    )
+    image_url = response_dallE['data'][0]['url']
+    print(image_url)
+    return {"Respuesta": response_chat, "Imagen": image_url}
